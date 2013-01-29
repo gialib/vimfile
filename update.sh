025bde94194378baff45928a1083a9ad2e8a65cb
WangJinzhong <andywang7259@gmail.com>
2013-01-29 22:07:16 +0800

    update sh

--- update.sh
+++ update.sh
@@ -1,2 +1,10 @@
+echo 'cd ~/.vim && git pull origin master'
+cd ~/.vim && git pull origin master
+
+echo 'cd ~/.vim/bundle/vundle && git pull origin master'
+cd ~/.vim/bundle/vundle && git pull origin master
+
+echo 'vim +BundleInstall +qall'
 vim +BundleInstall +qall
 
+echo 'updated successful!'
